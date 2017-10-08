7f2063c2e53fe727fff92b33dce440667b6aa550
zephylac <thibo.brocherieux@gmail.com>
2017-10-08 16:14:20 +0000

    image popup :+1:

--- static/js/custom.js
+++ static/js/custom.js
@@ -3,3 +3,23 @@ $(document).ready (function(){
   $(".alert").slideUp(500);
   });
 });
+
+// Get the modal
+var modal = document.getElementById('ModalImg');
+var modalImg = document.getElementById("img-content");
+var captionText = document.getElementById("img-caption");
+var span = document.getElementById("img-close");
+console.log(span);
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// on click on each image show in the above modal
+var imgs = document.getElementsByClassName("img-modal");
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
